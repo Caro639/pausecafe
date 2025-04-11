73b5a0b7cedd22ce41497a0bcd766892504eb2eb
Caro639 <120673222+Caro639@users.noreply.github.com>
2025-04-11 21:59:47 +0200

    ajout produit accueil+lien detail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
 use App\Repository\ProductsRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductsRepository $products): Response
+    public function index(ProductsRepository $repository, Products $product): Response
     {
 
         return $this->render(
             'home/index.html.twig',
             [
-                'products' => $products->findProduct(8),
+                'products' => $repository->findProduct(8),
+                'product' => $product,
+                'slug' => $product->getSlug(),
             ]
         );
     }
