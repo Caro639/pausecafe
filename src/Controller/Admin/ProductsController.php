ac39e5f3209ca14deef8e66849b88176bb93efee
Caro639 <120673222+Caro639@users.noreply.github.com>
2025-04-12 20:51:47 +0200

    ajout admin create+edit product

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProductsFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/produits', name: 'admin_products_')]
 class ProductsController extends AbstractController
@@ -18,19 +22,63 @@ class ProductsController extends AbstractController
         ]);
     }
     #[Route('/ajout', name: 'add.admin')]
-    public function add(): Response
-    {
+    public function add(
+        Request $request,
+        EntityManagerInterface $em,
+        SluggerInterface $slugger
+    ): Response {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig', [
-            'controller_name' => 'UsersController',
+
+        $product = new Products();
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+        $productForm->handleRequest($request);
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+
+            $prix = $product->getPrice();
+            $product->setPrice($prix);
+
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Le produit a bien été ajouté !');
+            return $this->redirectToRoute('admin_products_index.admin');
+        }
+
+        return $this->render('admin/products/add.html.twig', [
+            'productForm' => $productForm->createView(),
         ]);
     }
     #[Route('/edition/{id}', name: 'edit.admin')]
-    public function edit(Products $product): Response
-    {
+    public function edit(
+        Products $product,
+        Request $request,
+        EntityManagerInterface $em,
+        SluggerInterface $slugger
+    ): Response {
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
-        return $this->render('admin/products/index.html.twig', [
-            'controller_name' => 'UsersController',
+
+        $prix = $product->getPrice() / 100;
+        $product->setPrice($prix);
+
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+        $productForm->handleRequest($request);
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Le produit a bien été modifié !');
+            return $this->redirectToRoute('admin_products_index.admin');
+        }
+
+        return $this->render('admin/products/edit.html.twig', [
+            'productForm' => $productForm->createView(),
         ]);
     }
     #[Route('/suppression/{id}', name: 'delete.admin')]
---
+++ src/Form/ProductsFormType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Products;
+use App\Entity\Categories;
+use App\Repository\CategoriesRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProductsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom du produit',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Description',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('price', MoneyType::class, [
+                'divisor' => 100,
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Prix',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('stock', IntegerType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Stock',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class,
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'group_by' => 'parent.name',
+                'query_builder' => function (CategoriesRepository $cr) {
+                    return $cr->createQueryBuilder('c')
+                        ->where('c.parent IS NOT NULL')
+                        ->orderBy('c.name', 'ASC');
+                },
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-lg btn-dark mt-4'
+                ],
+                'label' => 'Valider'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
